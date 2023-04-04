31bc8656ec438e1ca06ec8463f4a366c9576e9dd
Kirill <kirillgroshev52@gmail.com>
2023-04-04 18:52:42 +0300

    Фикс слайдера. Приведение слайдера к образцу макета.

--- js/slider.js
+++ js/slider.js
@@ -4,33 +4,31 @@ sliders = [];
 for (let i = 0; i < containers.length; i++){
     sliders[i] = {
         position: 0,
-        slidesToShow: 6,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 3,
         container: containers[i],
         track: containers[i].querySelector(".slider-track"),
         btnPrev: containers[i].querySelector(".btn-prev"),
         btnNext: containers[i].querySelector(".btn-next"),
         items: containers[i].querySelectorAll(".slider-item"),
         itemsCount: containers[i].querySelectorAll(".slider-item").length,
-        itemWidth: (1250) / 10,
-        movePosition: 4 * ((1250) / 10),
+        itemWidth: (containers[i].clientWidth) / 5,
+        movePosition: 3 * ((containers[i].clientWidth) / 5),
         setPosition(){
             this.track.style.transform = `translateX(${this.position}px)`;
         },
         checkBtns(){
             this.btnPrev.disabled = this.position === 0;
-            this.btnNext.disables = this.position <= -(this.itemsCount - this.slidesToShow) * this.itemWidth;
+            this.btnNext.disabled = this.position <= -(this.itemsCount - this.slidesToShow) * this.itemWidth;
         },
     };
     
-    sliders[i].items.forEach((item) => {
-        //item.style.minWidth = `${sliders[i].itemWidth}px`;
-    });
-    
-    sliders[i].btnNext.addEventListener('click' , () => {
+    sliders[i].btnNext.addEventListener('click', () => {
         const itemsLeft = sliders[i].itemsCount - (Math.abs(sliders[i].position) + sliders[i].slidesToShow * sliders[i].itemWidth) / sliders[i].itemWidth;
         
-        sliders[i].position -= itemsLeft >= sliders[i].slidesToScroll? sliders[i].movePosition : itemsLeft * sliders[i].itemWidth;
+        sliders[i].position -= itemsLeft >= sliders[i].slidesToScroll? 
+            sliders[i].movePosition : 
+            itemsLeft * sliders[i].itemWidth;
 
         sliders[i].setPosition();
         sliders[i].checkBtns();
@@ -38,7 +36,10 @@ for (let i = 0; i < containers.length; i++){
     
     sliders[i].btnPrev.addEventListener('click', () => {
         const itemsLeft = Math.abs(sliders[i].position) / sliders[i].itemWidth;
-        sliders[i].position += itemsLeft >= sliders[i].slidesToScroll ? sliders[i].movePosition : itemsLeft * sliders[i].itemWidth;
+
+        sliders[i].position += itemsLeft >= sliders[i].slidesToScroll ? 
+            sliders[i].movePosition : 
+            itemsLeft * sliders[i].itemWidth;
 
         sliders[i].setPosition();
         sliders[i].checkBtns();
